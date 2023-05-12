8af510e54ecc6cef8d02fc7515a09e8fda025d3b
Alberto Nieto <94092485+KingMuerte@users.noreply.github.com>
2023-05-12 04:35:45 +0000

    Add Summary of PDF after attachment is uploaded Fixes #1

--- src/app.py
+++ src/app.py
@@ -1,13 +1,16 @@
 import streamlit as st
-from utils import parse_pdf, embed_text, get_answer
+
+from utils import embed_text, get_answer, get_summary, parse_pdf
 
 st.header("Doc QA")
 uploaded_file = st.file_uploader("Upload a pdf", type=["pdf"])
 
 if uploaded_file is not None:
+    st.write("Summary: ")
+    summary = get_summary(parse_pdf(uploaded_file))
+    st.write(summary)
     index = embed_text(parse_pdf(uploaded_file))
     query = st.text_area("Ask a question about the document")
     button = st.button("Submit")
     if button:
         st.write(get_answer(index, query))
-
